901212b47bdf2da0649034bae03896b2a5cb15a4
Wiktor Laskowski <117634264+wiktorlaskowski@users.noreply.github.com>
2025-06-28 19:41:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-const CACHE_NAME = 'egg-timer-cache-v1';
-const ASSETS = [
-  '/',
-  '/App/index.html',
-  '/App/favicon.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
-});
+const CACHE = 'egg-timer-v1';
+self.addEventListener('install', e =>
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        '/',
+        '/index.html',
+        '/favicon.png'
+      ])
+    )
+  )
+);
+self.addEventListener('fetch', e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
